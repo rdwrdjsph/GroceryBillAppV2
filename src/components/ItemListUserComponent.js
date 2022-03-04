36c77c364f96c43509ef73e63c50198d7384ad07
rdwrdjsph <94514177+rdwrdjsph@users.noreply.github.com>
2022-03-04 15:56:07 +0800

    Add files via upload

---
+++ src/components/ItemListUserComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import ItemService from '../services/ItemService';
+import { BiEdit, BiTrash, BiCheckCircle, BiXCircle} from "react-icons/bi";
+import { RiFileList2Line } from "react-icons/ri";
+
+class ItemListAdminComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items : []
+        }
+    }
+
+    componentDidMount() {
+        ItemService.getItem().then((res) => {
+            this.setState({ items: res.data});
+        });
+    }
+
+    discountedChecker(item)  {
+        if(item == true) {
+            return <BiCheckCircle/>
+        } else {
+            return <BiXCircle/>
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                <h2 className="text-center">Item List</h2>
+                <button className="btn btn-secondary" onClick={this.addItem}><RiFileList2Line /> Generate Bill</button>
+                </div><br/>
+                <div className="row table-responsive">
+                    <table className="table table-hover text-center table-light">
+                        <thead className="table-dark">
+                            <tr>
+                                <th>Item ID</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Discounted Price</th>
+                                <th>Discounted?</th>
+                                <th>Discount %</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.items.map(
+                                    item =>
+                                    <tr key = {item.itemId}>
+                                        <td>{item.itemId}</td>
+                                        <td>{item.name}</td>
+                                        <td>&#8369;{item.price}</td>
+                                        <td>&#8369;{item.discountedPrice}</td>
+                                        <td>{this.discountedChecker(item.isDiscounted)}</td>
+                                        <td>{item.discountPercentage}%</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(ItemListAdminComponent);
