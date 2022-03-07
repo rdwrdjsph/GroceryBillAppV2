e9a363a554b969117ba949f79bba67630bddfd6d
rdwrdjsph <94514177+rdwrdjsph@users.noreply.github.com>
2022-03-07 17:32:45 +0800

    Delete src directory

--- src/services/AuthHeader.js
+++
@@ -1,8 +0,0 @@
-export default function AuthHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.token) {
-    return { Authorization: 'Bearer ' + user.token };
-  } else {
-    return {};
-  }
-}
